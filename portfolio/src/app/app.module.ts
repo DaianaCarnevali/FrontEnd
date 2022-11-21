16ddbd75920934c47a44d888186e68cdb3a0b305
Daiana Carnevali <116859046+DaianaCarnevali@users.noreply.github.com>
2022-11-21 13:44:52 -0300

    componentes header y banner creado

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SwiperModule } from 'swiper/angular';
+import { NgxTypedJsModule } from 'ngx-typed-js';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ResumenComponent } from './resumen/resumen.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InicioComponent,
+    ResumenComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    HabilidadesComponent,
+    ContactoComponent,
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SwiperModule,
+    NgxTypedJsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
